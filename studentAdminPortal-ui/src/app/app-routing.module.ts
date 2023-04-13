811909e663acc892e2dfd462d2567e157681382e
shaheb19aug <tarakanta.das@iteos.in>
2023-04-13 11:50:33 +0530

    Initial

--- studentAdminPortal-ui/src/app/app-routing.module.ts
+++ studentAdminPortal-ui/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
+import { ServiceComponent } from './students/service/service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:ServiceComponent
+  },
+  {
+    path:'students',
+    component:StudentsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
